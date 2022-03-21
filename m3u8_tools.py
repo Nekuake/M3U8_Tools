766c0ef74c6dbbded6df721c6e4919d309f79d65
Nekuake <ruizale55@gmail.com>
2022-03-21 01:26:33 +0100

    Imnproved continuous checking. Custom function calling working.

--- m3u8_tools.py
+++ m3u8_tools.py
@@ -1,6 +1,6 @@
+import time
 import urllib.error
 import m3u8
-import requests
 
 
 def check_stream_connection(m3u8_url):
@@ -12,10 +12,15 @@ def check_stream_connection(m3u8_url):
         return False
 
 
-def check_stream_run(m3u8_url, code, arguments, run_on_down):
-    if not check_stream_connection(m3u8_url):
-        if run_on_down:
-            code(arguments)
-    else:
-        if not run_on_down:
-            code(arguments)
+def continuous_check_stream_run(m3u8_url, code, arguments, run_on_down, time_between_tries, continue_on_fail):
+    while True:
+        if not check_stream_connection(m3u8_url):
+            if run_on_down:
+                code(arguments)
+            if continue_on_fail:
+                return None
+        else:
+            if not run_on_down:
+                code(arguments)
+        time.sleep(time_between_tries)
+
