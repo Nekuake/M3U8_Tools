dcd0d2dda7074489dc52242815e4a6f9e9bce407
Nekuake <ruizale55@gmail.com>
2022-03-21 01:39:39 +0100

    Changed function name to continue_on_trigger. More consistent.

--- m3u8_tools.py
+++ m3u8_tools.py
@@ -12,15 +12,18 @@ def check_stream_connection(m3u8_url):
         return False
 
 
-def continuous_check_stream_run(m3u8_url, code, arguments, run_on_down, time_between_tries, continue_on_fail):
+def continuous_check_stream_run(m3u8_url, code, arguments, run_on_down, time_between_tries, continue_on_trigger):
     while True:
         if not check_stream_connection(m3u8_url):
             if run_on_down:
                 code(arguments)
-            if continue_on_fail:
+            if not continue_on_trigger:
                 return None
         else:
             if not run_on_down:
                 code(arguments)
+            if not continue_on_trigger:
+                return None
+
         time.sleep(time_between_tries)
 
